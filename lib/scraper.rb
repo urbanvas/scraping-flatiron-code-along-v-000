49ae7a15d0ab57029dff1c51b1807620dddec32a
Neil Seejoor <nvseejoor@gmail.com>
2018-02-27 23:25:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+    def get_page
+
+    end
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +19,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
